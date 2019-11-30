948ddb64201cca8eb8a2f9e7c4889b8fffeb0cfe
twr143 <twr143@gmail.com>
2019-11-30 21:08:50 +0300

    http4s version upgrade to 0.2015, 2

--- src/main/scala/io/github/spf3000/hutsapi/HutRepository.scala
+++ src/main/scala/io/github/spf3000/hutsapi/HutRepository.scala
@@ -1,5 +1,4 @@
 package io.github.spf3000.hutsapi
-
 import java.util.UUID
 import cats.effect._
 import scala.collection.mutable.ListBuffer
@@ -7,41 +6,57 @@ import cats.FlatMap
 import cats.implicits._
 import cats.effect.IO
 import io.github.spf3000.hutsapi.entities._
-
 import java.util.concurrent.Executors
 import scala.concurrent.ExecutionContext
 
-final case class HutRepository[F[_]](private val huts: ListBuffer[HutWithId])(implicit e: Effect[F]) {
+final case class HutRepository[F[_]](private val huts: ListBuffer[HutWithId])
+                                    (implicit e: Effect[F], cs: ContextShift[F], timer: Timer[F]) {
 
   val blockingEC = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(4))
-  val makeId: F[String] = e.delay { UUID.randomUUID().toString }
+
+  val makeId: F[String] = e.delay {
+    UUID.randomUUID().toString
+  }
 
   def getAll: F[List[HutWithId]] =
-    e.delay { huts.toList }
-
-    def getHut(id: String): F[Option[HutWithId]] =
-      e.delay { huts.find(_.id == id) }
-
-    def addHut(hut: Hut): F[String] =
-      for {
-        uuid <- makeId
-        _ <- e.delay { huts += hutWithId(hut, uuid) }
-      } yield uuid
-
-    def updateHut(hutWithId: HutWithId): F[Unit] = {
-      for {
-        _ <- e.delay { huts.find(_.id == hutWithId.id).foreach(h => huts -= h) }
-        _ <- e.delay { huts += hutWithId }
-      } yield()
+    e.delay {
+      huts.toList
     }
 
-    def deleteHut(hutId: String): F[Unit] =
-      e.delay { huts.find(_.id == hutId).foreach(h => huts -= h) }
+  def getHut(id: String): F[Option[HutWithId]] =
+    e.delay {
+      huts.find(_.id == id)
+    }
 
+  def addHut(hut: Hut): F[String] =
+    for {
+      uuid <- makeId
+      _ <- e.delay {
+        huts += hutWithId(hut, uuid)
+      }
+    } yield uuid
 
-    def hutWithId(hut: Hut, id: String): HutWithId =
-      HutWithId(id, hut.name)
+  def updateHut(hutWithId: HutWithId): F[Unit] = {
+    for {
+      _ <- e.delay {
+        huts.find(_.id == hutWithId.id).foreach(h => huts -= h)
+      }
+      _ <- e.delay {
+        huts += hutWithId
+      }
+    } yield ()
+  }
+
+  def deleteHut(hutId: String): F[Unit] =
+    e.delay {
+      huts.find(_.id == hutId).foreach(h => huts -= h)
+    }
+
+  def hutWithId(hut: Hut, id: String): HutWithId =
+    HutWithId(id, hut.name)
 }
+
 object HutRepository {
-  def empty[F[_]](implicit m: Effect[F]): HutRepository[F] = new HutRepository[F](ListBuffer())
+
+  def empty[F[_]](implicit m: Effect[F], cs: ContextShift[F], timer: Timer[F]): HutRepository[F] = new HutRepository[F](ListBuffer())
 }
